ed74c8e6a3ed0accd3d5ed04d6756644a7aef7e9
aadig <spider.guya360@gmail.com>
2023-11-25 10:51:56 +0000

    Initial commit

---
+++ src/main/java/net/aadi/mccourse/MCCourseMod.java
@@ -0,0 +1,18 @@
+package net.aadi.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+    public static final Logger LOGGER = LoggerFactory.getLogger("mc-course");
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
