a61bee018fe6500f95c7d8a1176e695f8b475a5b
aadig <spider.guya360@gmail.com>
2023-11-25 16:31:08 +0000

    NewItem

--- src/main/java/net/aadi/mccourse/MCCourseMod.java
+++ src/main/java/net/aadi/mccourse/MCCourseMod.java
@@ -1,5 +1,6 @@
 package net.aadi.mccourse;
 
+import net.aadi.mccourse.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class MCCourseMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
